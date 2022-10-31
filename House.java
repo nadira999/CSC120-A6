5409dc782f5eb33d8795d7c3f5f87e36855a028b
nadira999 <ntalayee@smith.edu>
2022-10-30 22:16:01 -0400

    House class completed! will work on the rest.

--- House.java
+++ House.java
@@ -1,12 +1,39 @@
-/* This is a stub for the House class */
-public class House {
+import java.util.ArrayList;
 
-  public House() {
+
+/* This is a stub for the House class */
+public class House extends Building{
+
+  public House(String name, String address,int nFloors, boolean hasDinngRoom) {
+    super(name, address, nFloors);
     System.out.println("You have built a house: 🏠");
+    private ArrayList<String> residents;
+    private boolean dinngRoom;
+  
+      
+    
+    
+   
+    }
+    public void moveInResident(String resname){
+       resname = new resname; 
+
+
+  
+    }
+
+    public void moveOutResidents(){
+        
+
+    }
+    public String getresidents(){
+      return this.residents = new residents;
+}
   }
 
   public static void main(String[] args) {
-    new House();
+  House Albright = new House("Albright House", "1 Chapin Way", 5, true);
+
   }
 
 }
